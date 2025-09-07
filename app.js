68fd8e8936fa4bee3bfa1accc6bb15ccacb2f25d
Anaxímenes Nascimento <anaximenesnascimento11@gmail.com>
2025-09-06 21:37:59 -0300

    Inclusão da função de sortear o amigo secreto.

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
-var listaNomesAmigos=[];
+let listaNomesAmigos=[];
 
 function adicionarAmigo() {
-    var nomeDoAmigo=document.querySelector('input').value
+    let nomeDoAmigo=document.querySelector('input').value
     if(nomeDoAmigo===''){
         alert('Por favor, insira um nome.');
-    } else if (listaNomesAmigos.includes(nomeDoAmigo)){
+    } else if (listaNomesAmigos.some(a => a.toLowerCase() ===nomeDoAmigo.toLowerCase())){
         alert('Esse nome já foi inserido. Por favor insira outro nome.')
     } else{
         listaNomesAmigos.push(nomeDoAmigo);
     }
+    exibirAmigos();
     console.log(listaNomesAmigos);
     document.querySelector('input').value="";
 }
 
-function atualizarListaNomes() {
+function exibirAmigos() {
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    for (let i = 0; i < listaNomesAmigos.length; i++) {
+         lista.innerHTML += `<li>${listaNomesAmigos[i]}</li>`;
+    }
+}
+
+function sortearAmigo() {
+    const resultado = document.getElementById('resultado');
+    if (listaNomesAmigos.length === 0){
+        alert('Por favor, insira ao menos um amigo!');
+        return;
+    }
+    const indice = Math.floor(Math.random()*listaNomesAmigos.length);
+    const sorteado = listaNomesAmigos[indice];
+    resultado.innerHTML = `O nome do seu amigo secreto é ${sorteado}!`;
     
 }
